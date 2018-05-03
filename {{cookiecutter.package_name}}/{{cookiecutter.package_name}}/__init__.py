0845cc4c621e92b90c50ede1030b0018b04054fe
daijian@air <daijian@flowself.com>
2018-05-03 09:09:28 +0800

    improve env loading

--- {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/__init__.py
@@ -8,8 +8,4 @@ log_config.set_logging(os.path.join(BASE_DIR, 'logs'))
 app = Flask('{{cookiecutter.package_name}}')
 app.config.from_object('{{cookiecutter.package_name}}.default_settings')
 
-import dotenv
-
-dotenv.load_dotenv(os.path.join(BASE_DIR, '.env'))
-
 import {{cookiecutter.package_name}}.views  # Must be loaded at last
